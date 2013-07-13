9c303de09462646722f7060be1b1f6648b3f0fd7
Chris McCoy <chris@brownbagcode.com>
2013-07-13 06:37:41 -0400

    updating the script to install amqp

--- vagrant_getting_started/php/install-amqp.sh
+++ vagrant_getting_started/php/install-amqp.sh
@@ -1,12 +1,15 @@
 cd /tmp
+sudo rm -r /tmp/rabbitmq-c
 git clone git://github.com/alanxz/rabbitmq-c.git
 cd /tmp/rabbitmq-c
 git submodule init
 git submodule update
+git checkout tags/rabbitmq-c-v0.3.0
 sudo autoreconf -i
 sudo ./configure
 sudo make
 sudo make install
+sudo pecl uninstall amqp
 sudo pecl install amqp
 sudo cp /vagrant/amqp.ini /etc/php5/conf.d/amqp.ini
 sudo /etc/init.d/php5-fpm restart
