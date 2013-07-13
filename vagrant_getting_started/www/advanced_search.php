5676c722d3d01e8e0879d5153f589a7c28c69612
jnoble <jessica.noble11@gmail.com>
2013-07-13 12:31:17 -0400

    headers and footers

---
+++ vagrant_getting_started/www/advanced_search.php
@@ -0,0 +1,20 @@
+<h2>Advanced Search</h2>
+<form>
+<div>
+Customer ID:
+<input type="text" name="CSNUtID" />
+</div>
+<div>
+Time from
+<input type="text" name="dateFrom" id="dateFrom"/>
+to
+<input type="text" name="dateTo" id="dateTo"/>
+</div>
+<div>
+Process:
+<input type="text" name="processName" />
+</div>
+<div>
+Parent Transaction:
+<input type="text" name="parentTrans" />
+</div>
--- vagrant_getting_started/www/index.php
+++ vagrant_getting_started/www/index.php
@@ -1,53 +1,28 @@
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link rel="stylesheet" href="awesome10.css" type="text/css" />
-
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
- <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-<script>
-$(document).ready(function(){
-                  
-                  $.ajax({
-                         url: 'advanced_search.php',
-                         dataType: 'html'
-                  }).success(function(responseText) {
-                             $('#advanced_search_content').html(responseText);
-                             var dateFrom = $('#dateFrom'),
-                             dateTo = $('#dateTo');
-                  
-                             dateFrom.datepicker();
-                             dateTo.datepicker();
-                             });
-                  });
-</script>
-
-<title>Logging Test</title>
-</head>
-<body>
 <?php
+  include "awesome_header.php";
   ini_set('display_errors', E_ALL);
-    
-    $keyword = '';
-    if (isset($_REQUEST['keyword'])) {
-        $keyword = $_REQUEST['keyword'];
-    }
-?>
+  
+  $keyword = '';
+  if (isset($_REQUEST['keyword'])) {
+    $keyword = $_REQUEST['keyword'];
+  }
+  ?>
 <h1>Team Awesome's Amazing Logomatic Machine</h1>
 <a title="run" class="run_button">RUN</a
 <form>
-    <input type="text" name="keyword" value="<?$keyword?>" />
-    <button type="submit" id="search_button" title="search">SEARCH</button>
+<input type="text" name="keyword" value="<?$keyword?>" />
+<button type="submit" id="search_button" title="search">SEARCH</button>
 </form>
 
-Number of results:  
+Number of results:
 <select>
-    <option value="10" selected="true">10</option>
-    <option value="25" selected="false">25</option>
-    <option value="50" selected="false">50</option>
-    <option value="100" selected="false">100</option>
+<option value="10" selected="true">10</option>
+<option value="25" selected="false">25</option>
+<option value="50" selected="false">50</option>
+<option value="100" selected="false">100</option>
 </select>
-<a href="" id="js_advanced_search">Advanced Search</a>
-<span id="advanced_search_content"></span>
+<a id="js_advanced_search">Advanced Search</a>
+<div width="300px" style="background-color:#BCE1F5;" id="advanced_search_content"></div>
 <table name="results">
 <tr>
 <th>CSNUtID</th>
@@ -64,4 +39,6 @@ Number of results:
 <td>TEST</td>
 </tr>
 </table>
-</body>
+<?
+include "awesome_footer.php";
+?>
