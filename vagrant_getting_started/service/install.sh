7ee22ff747d1b1962cb4b3e491dbe7542914f711
Chris McCoy <chris@brownbagcode.com>
2013-07-12 22:45:56 -0400

    adding some more install steps for service box

--- vagrant_getting_started/service/install.sh
+++ vagrant_getting_started/service/install.sh
@@ -6,6 +6,8 @@ sudo apt-get install php5
 sudo dpkg -i /vagrant/nginx-latest.deb
 sudo rm /etc/nginx/*
 sudo cp /vagrant/service/nginx/* /etc/nginx/
-sudo mkdir /opt/logs
-sudo mkdir /var/log/nginx
+test -d /opt/logs ||  sudo mkdir /opt/logs
+test -d /var/log/nginx || sudo mkdir /var/log/nginx
+test -d /var/www || sudo mkdir /var/www
+sudo chown -R www-data /var/www
 sudo /etc/init.d/nginx restart
