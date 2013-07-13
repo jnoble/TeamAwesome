4bf4146e4bd9c255acdd025538eea503819bfba9
Chris McCoy <chris@brownbagcode.com>
2013-07-12 23:00:42 -0400

    adding install data for php

---
+++ vagrant_getting_started/php/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. ../common.sh
+
+sudo apt-get install php5-fpm
+sudo apt-get install php5
+sudo dpkg -i /vagrant/nginx-latest.deb
+sudo rm /etc/nginx/*
+sudo cp /vagrant/php/nginx/* /etc/nginx/
+test -d /opt/logs ||  sudo mkdir /opt/logs
+test -d /var/log/nginx || sudo mkdir /var/log/nginx
+test -d /var/www || sudo mkdir /var/www
+sudo chown -R www-data /var/www
+sudo /etc/init.d/nginx restart
