10608a2f2ef85bf08ee3b378ee4ccfa11f65728f
Chris McCoy <chris@brownbagcode.com>
2013-07-12 23:00:09 -0400

    wrong path to nginx data

--- vagrant_getting_started/lb/install.sh
+++ vagrant_getting_started/lb/install.sh
@@ -6,7 +6,7 @@ sudo apt-get install haproxy
 sudo dpkg -i /vagrant/nginx-latest.deb
 sudo cp /vagrant/lb/haproxy.cfg /etc/haproxy/
 sudo rm /etc/nginx/*
-sudo cp /vagrant/service/nginx/* /etc/nginx/
+sudo cp /vagrant/lb/nginx/* /etc/nginx/
 test -d /opt/logs ||  sudo mkdir /opt/logs
 test -d /var/log/nginx || sudo mkdir /var/log/nginx
 test -d /var/www || sudo mkdir /var/www
