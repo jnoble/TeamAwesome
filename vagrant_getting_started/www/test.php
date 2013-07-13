9cd6ba6f8e1ee18f1a406fd2afc2c30a8aa0353c
bret.aarden <bret.aarden@gmail.com>
2013-07-13 13:28:49 -0400

    Broken function pieces

--- vagrant_getting_started/www/test.php
+++ vagrant_getting_started/www/test.php
@@ -1,28 +1,5 @@
 <?php
-
-ini_set('display_errors', E_ALL);
-set_include_path(get_include_path() . PATH_SEPARATOR . '/vagrant/www');
-
-require 'log4php/Logger.php';
-require 'log4php/appenders/LoggerAppenderAMQP.php';
-require 'log4php/layouts/LoggerLayoutGelf.php';
-
-// Get or set user ID cookie
-$_SESSION['CSNUtID'] = isset($_COOKIE['CSNUtID']) ? $_COOKIE['CSNUtID'] : '';
-if (!$_SESSION['CSNUtID']) {
-   $_SESSION['CSNUtID'] = uniqid();
-   $expiration = time() + (3 * 365 * 24 * 60 * 60); // 3 years
-   setcookie('CSNUtID', $_SESSION['CSNUtID'], $expiration, '/');
-}
-// Get transaction IDs
-$_SESSION['HTTP_X_REQUEST_ID'] = isset($_SERVER['HTTP_X_REQUEST_ID']) ? $_SERVER['HTTP_X_REQUEST_ID'] : '';
-$_SESSION['PARENT_REQUEST_ID'] = isset($_REQUEST['PARENT_REQUEST_ID']) ? $_REQUEST['PARENT_REQUEST_ID'] : '';
-$_SESSION['BROWSE_DEPTH'] = isset($_REQUEST['BROWSE_DEPTH']) ? $_REQUEST['BROWSE_DEPTH'] : 1;
-if (! $_SESSION['PARENT_REQUEST_ID']) {
-  $_SESSION['BROWSE_DEPTH']++;
-}
-
-server_log('PHP', $_SERVER['PHP_SELF']);
+require 'header.php';
 
 $visits = 0;
 //$sql = 'DROP TABLE tblLog';
@@ -53,23 +30,12 @@ echo 'Transaction: ' . htmlentities($_SESSION['HTTP_X_REQUEST_ID']) . '<br />';
 echo 'User ID: ' . htmlentities($_SESSION['CSNUtID']) . '<br />';
 echo 'Prior number of visits: ' . htmlentities($visits) . '<br />';
 
-$query = '';
+    $request = array('type' => 'user_id', 'key' => $_SESSION['CSNUtID']);
+    $request = array('type' => 'transaction_id', 'key' => $_SESSION['HTTP_X_REQUEST_ID']);
+$result = curl('http://' . SERVICE . '/elasticsearch_service.php', json_encode($request));
+echo '<br />Results:<br />' . var_export($result, true);
 
 
-function server_log($type, $request) {
-  Logger::configure('log4php/config.xml');
-  $logger = Logger::getLogger("main");
-  $server_data = array('CSNUtID' => $_SESSION['CSNUtID'],
-                       'HTTP_X_REQUEST_ID' => $_SESSION['HTTP_X_REQUEST_ID'],
-                       'PARENT_REQUEST_ID' => $_SESSION['PARENT_REQUEST_ID'],
-                       'BROWSE_DEPTH' => $_SESSION['BROWSE_DEPTH'],
-                       'SERVER' => $type,
-                       'REQUEST' => $request,
-                       'TIMESTAMP' => date(DATE_ATOM));
-  $logger->info(json_encode($server_data));
-}
-
-    
 function query($sql) {
   $mysqli = mysqli_connect('localhost', 'root', 'guest', 'mydata');
   $res = mysqli_query($mysqli, $sql);
@@ -78,17 +44,6 @@ function query($sql) {
   server_log('MySQL', $sql);
 
   return $res;
-}
-
-function quote($value, $type) {
-  if ($type == 0) {
-    return "'" . $value . "'";
-  }
-  return $value;
-}
-
-function pretend_nothing_happened() {
-  ;
 }
 
 ?>
