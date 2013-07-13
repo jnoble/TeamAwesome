5e8adf5ea5d8deb0dfd28d5b16accd9edd5ee278
Chris McCoy <chris@brownbagcode.com>
2013-07-13 18:00:22 -0400

    adding the detail page for txid and csnutids

---
+++ vagrant_getting_started/www/view_detail.php
@@ -0,0 +1,113 @@
+<?php
+  include "awesome_header.php";
+  include "header.php";
+  ini_set('display_errors', E_ALL);
+
+  $csnutid = $_REQUEST['csnutid'];
+  $txid = $_REQUEST['txid'];
+  $page_type = null;
+  if ( !empty($csnutid) ) {
+    //we're going to be making a list of transactions
+    $page_type = 'tx_report';
+  } elseif ( !empty($txid) ) {
+    $page_type = 'stack_report';
+  }
+  $num_result = isset($_REQUEST['num_results']) ? $_REQUEST['num_results'] : 10;
+
+  ?>
+<h1>Team Awesome's Amazing Logomatic Machine</h1>
+
+<div style="width:900px;height:50px;background-color:#BCE1F5">
+<br>
+<div style="float:right;vertical-align:middle">
+Number of results:
+<select name="num_results">
+<option value="10" selected="true">10</option>
+<option value="25" selected="false">25</option>
+<option value="50" selected="false">50</option>
+<option value="100" selected="false">100</option>
+</select>
+</div>
+<br><br>
+</div>
+<br>
+<table name="results" style="float:left;width:900px;">
+<?php
+/*
+{
+  "query": {
+    "term": {
+      "CSNUtID": "51e11b77dcbb6"
+    }
+  },
+  "sort": [
+    {
+      "@timestamp": {
+        "order": "desc"
+      }
+    }
+  ],
+  "size": size
+}
+*/
+  if ($page_type ==  'tx_report') {
+    $term = array( "CSNUtID" => $csnutid );
+  } else {
+    $term = array( "txid" => $txid );
+  }
+  $query = array(
+    "query" =>
+      array("term" => $term ),
+    "sort" => 
+      array("@timestamp" => array( "order" => "desc" )),
+    "size" => $num_result);
+
+   $index = "clickstream-" . date("Y.m.d"); 
+   $data = curl("http://" . ELASTICSEARCH . ":9200/" . $index . "/_search",
+             json_encode($query));
+
+?>
+<tr>
+<?php
+if ($page_type == 'tx_report') {
+?>
+<th>Timestamp</th>
+<th>txid</th>
+<?php
+} else {
+?>
+<th>Timestamp</th>
+<th>Server</th>
+<th>Request</th>
+<th>User Agent</th>
+<th>CSNUtID</th>
+<?php
+}
+?>
+</tr>
+<tr>
+<?php
+$data_array = json_decode($data, true);
+if ($page_type == 'tx_report') {
+  foreach ($data_array['hits']['hits'] as $hit) {
+    $hit = $hit['_source'];
+    echo "<td>" . $hit['@timestamp'] . "</td>\n";
+    echo "<td><a href='/view_detail.php?txid=" . $hit['@fields']['txid'] . "'>" . $hit['@fields']['txid'] . "</a></td>\n";
+    echo "<tr>";
+  }
+} else {
+  foreach ($data_array['hits']['hits'] as $hit) {
+    $hit = $hit['_source'];
+    echo "<td>" . $hit['@timestamp'] . "</td>\n";
+    echo "<td>" . $hit['@source'] . "</td>\n";
+    echo "<td>" . $hit['@fields']['request'] . "</td>\n";
+    echo "<td>" . $hit['@fields']['http_user_agent'] . "</td>\n";
+    echo "<td><a href='/view_detail.php?csnutid=" . $hit['@fields']['CSNUtID'] . "'>" . $hit['@fields']['CSNUtID'] . "</a></td>\n";
+    echo "<tr>";
+  }
+}
+?>
+</table>
+<?
+include "awesome_footer.php";
+?>
