7c3d22117e2b7936597d4d3066d47c47868791fa
Chris McCoy <chris@brownbagcode.com>
2013-07-13 08:50:25 -0400

    adding beaver to all the types

--- vagrant_getting_started/common.sh
+++ vagrant_getting_started/common.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+set +e
 sudo apt-get install vim
 sudo apt-get install git-core
+sudo apt-get install python-pip
+
+/usr/bin/pip freeze | grep -i beaver > /dev/null 2>&1 || sudo /usr/bin/pip install git+git://github.com/josegonzalez/beaver.git#egg=beaver
+
+PIDFILE=/tmp/beaver.pid
+
+function beaver_status() {
+        test ! -f $PIDFILE && echo "beaver not running" && return 1
+        ps aux | grep $(cat $PIDFILE) | grep python >/dev/null&& echo "beaver running"
+}
+
+function kill_beaver() {
+        echo "killing beaver"
+        beaver_status >/dev/null && kill $(cat $PIDFILE) > /dev/null 2>&1|| return 0
+}
+
+function start_beaver() {
+        beaver_status || echo "starting beaver" && /usr/local/bin/beaver -F json -P $PIDFILE -t rabbitmq -c /tmp/beaver.ini --fqdn -D
+        sleep 1
+}
