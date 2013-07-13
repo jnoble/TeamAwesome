d0fd4c9b44fa13b4d3f3fefcd05aaa910d104b48
Chris McCoy <chris@brownbagcode.com>
2013-07-13 17:59:21 -0400

    installing php5-curl on php serves

--- vagrant_getting_started/php/install.sh
+++ vagrant_getting_started/php/install.sh
@@ -6,8 +6,10 @@ sudo apt-get install php5
 sudo apt-get install php-pear
 sudo apt-get install pkg-config
 sudo apt-get install logrotate
+sudo apt-get install php5-curl
 sudo crontab -l | grep "logrotate" > /dev/null || sudo crontab /vagrant/php/logrotate.cron
 sudo cp /vagrant/php/logrotate.d/nginx /etc/logrotate.d/
+sudo cp -r /vagrant/php/php_extensions/* /etc/php5/conf.d/
 sudo dpkg -i /vagrant/nginx-latest.deb
 sudo rm /etc/nginx/*
 sudo cp /vagrant/php/nginx/* /etc/nginx/
@@ -16,5 +18,6 @@ test -d /var/log/nginx || sudo mkdir /var/log/nginx
 test -d /var/www || sudo mkdir /var/www
 sudo chown -R www-data /var/www
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/php5-fpm restart
 diff /vagrant/php/beaver.ini /tmp/beaver.ini || kill_beaver && cp /vagrant/php/beaver.ini /tmp/beaver.ini
 start_beaver
