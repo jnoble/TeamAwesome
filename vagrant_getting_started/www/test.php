36d485629146014c121b7aa2b63a6ff6a81c94d0
bret.aarden <bret.aarden@gmail.com>
2013-07-13 00:57:48 -0400

    Added log4php/config.xml.

--- vagrant_getting_started/www/test.php
+++ vagrant_getting_started/www/test.php
@@ -4,8 +4,10 @@ ini_set('display_errors', E_ALL);
 set_include_path(get_include_path() . PATH_SEPARATOR . '/vagrant/www');
 
 include('log4php/Logger.php');
+Logger::configure('log4php/config.xml');
 
 $logger = Logger::getLogger("main");
+$logger->info("This is an informational message.");
 
 echo 'Test!';
 
