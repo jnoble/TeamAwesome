afc55b7ef29d690645f9a954e3c407d5e6b7ae93
jnoble <jessica.noble11@gmail.com>
2013-07-14 15:39:34 -0400

    bit of organization...

--- vagrant_getting_started/www/index.php
+++ vagrant_getting_started/www/index.php
@@ -1,64 +1,7 @@
 <?php
+  include 'awesome_header.php';
   include 'header.php';
-  include "awesome_header.php";
   ini_set('display_errors', E_ALL);
-    
+  include 'view_detail.php';
+  include 'awesome_footer.php';
   ?>
-<h1>Team Awesome's Amazing Logomatic Machine</h1>
-
-<div style="width:900px;height:50px;background-color:#BCE1F5">
-<br>
-<form style="float:left;vertical-align:middle">
-<select name="search_type">
-  <option value="customer">Customer ID</option>
-  <option value="transaction">TransactionID</option>
-</select>
-
-<input type="text" name="keyword" value="<?$keyword?>" />
-<button type="submit" id="search_button" title="search">SEARCH</button>
-</form>
-<div style="float:right;vertical-align:middle">
-Number of results:
-<select name="num_results">
-<option value="10" selected="true">10</option>
-<option value="25" selected="false">25</option>
-<option value="50" selected="false">50</option>
-<option value="100" selected="false">100</option>
-</select>
-</div>
-<br><br>
-</div>
-<br>
-<table name="results" style="float:left;width:900px;">
-<tr>
-<th>CSNUtID</th>
-<th>Name</th>
-<th>Timestamp</th>
-<th>Request</th>
-<th>Transaction ID</th>
-</tr>
-<tr>
-<?
-$url = 'http://' . SERVICE . '/elasticsearch_service.php';
-$query = array('key' => 'transaction_list', 'value' => '');
-$results = curl($url, json_encode($query));
-$results = json_decode($results);
-echo var_export($results, true);
-$mysql_url = 'http://' . SERVICE . '/mysql_service.php';
-foreach ($results as $row) {
-    $query = array('CSNUtID' => $row['CSNUtID']);
-    $name = curl($mysql_url, json_encode($query));
-    $name = json_decode($name);
-    echo '<td><a href="customer_list.php?CSNUtID=' . htmlentities($row['CSNUtID']) . '">'
-      . htmlentities($row['CSNUtID']) . '</a></td>';
-    echo '<td>' . htmlentities($name['First']) . ' ' . htmlentities($name['Last']) . '</td>';
-    echo '<td>' . htmlentities($row['timestamp']) . '</td>';
-    echo '<td>' . htmlentities($row['request']) . '</td>';
-    echo '<td>' . htmlentities($row['transaction']) . '</td>';
-}
-?>
-</tr>
-</table>
-<?
-include "awesome_footer.php";
-?>
--- vagrant_getting_started/www/view_detail.php
+++ vagrant_getting_started/www/view_detail.php
@@ -1,24 +1,52 @@
 <?php
-  include "awesome_header.php";
-  include "header.php";
-  ini_set('display_errors', E_ALL);
-
-  $csnutid = $_REQUEST['csnutid'];
-  $txid = $_REQUEST['txid'];
-  $page_type = null;
-  if ( !empty($csnutid) ) {
-    //we're going to be making a list of transactions
-    $page_type = 'tx_report';
-  } elseif ( !empty($txid) ) {
-    $page_type = 'stack_report';
-  }
+  
+  $keyword = '';
+  $data_key = '';
+  $num_results = '';
+  $search_button = '';
   $num_result = isset($_REQUEST['num_results']) ? $_REQUEST['num_results'] : 10;
-
+  $page_type = '';
+  $search_button = isset($_REQUEST['search_button'])?$_REQUEST['search_button']:'';
+  
+  if (!empty($search_button)) {
+    
+    $keyword = $_REQUEST['keyword'];
+    $data_key = $_REQUEST['search_type'];
+    
+    if ($data_key = 'customer') {
+      $page_type = 'tx_report';
+    } else {
+      $page_type = 'stack_report';
+    }
+  }
+  if ($page_type ==  'tx_report') {
+    $term = array( "CSNUtID" => $csnutid );
+  } else {
+    $term = array( "txid" => $txid );
+  }
+  $query = array(
+                 "query" =>
+                 array("term" => $keyword ),
+                 "sort" =>
+                 array("@timestamp" => array( "order" => "desc" )),
+                 "size" => $num_result);
+  
+  $index = "clickstream-" . date("Y.m.d");
+  $data = curl("http://" . ELASTICSEARCH . ":9200/" . $index . "/_search",
+               json_encode($query));
   ?>
 <h1>Team Awesome's Amazing Logomatic Machine</h1>
 
 <div style="width:900px;height:50px;background-color:#BCE1F5">
 <br>
+<form style="vertical-align:middle">
+<select name="search_type">
+<option value="customer">Customer ID</option>
+<option value="transaction">TransactionID</option>
+</select>
+
+<input type="text" name="keyword" value="<?$keyword?>" />
+<button type="submit" id="search_button" title="search">SEARCH</button>
 <div style="float:right;vertical-align:middle">
 Number of results:
 <select name="num_results">
@@ -28,86 +56,81 @@ Number of results:
 <option value="100" selected="false">100</option>
 </select>
 </div>
+</form>
 <br><br>
 </div>
 <br>
 <table name="results" style="float:left;width:900px;">
 <?php
-/*
-{
-  "query": {
-    "term": {
-      "CSNUtID": "51e11b77dcbb6"
-    }
-  },
-  "sort": [
-    {
-      "@timestamp": {
-        "order": "desc"
-      }
-    }
-  ],
-  "size": size
-}
-*/
-  if ($page_type ==  'tx_report') {
-    $term = array( "CSNUtID" => $csnutid );
-  } else {
-    $term = array( "txid" => $txid );
-  }
-  $query = array(
-    "query" =>
-      array("term" => $term ),
-    "sort" => 
-      array("@timestamp" => array( "order" => "desc" )),
-    "size" => $num_result);
-
-   $index = "clickstream-" . date("Y.m.d"); 
-   $data = curl("http://" . ELASTICSEARCH . ":9200/" . $index . "/_search",
-             json_encode($query));
-
-?>
+  /*
+   {
+   "query": {
+   "term": {
+   "CSNUtID": "51e11b77dcbb6"
+   }
+   },
+   
+   "sort": [
+   {
+   "@timestamp": {
+   "order": "desc"
+   }
+   }
+   ],
+   "size": size
+   }
+   */
+  
+  
+  
+  $data_array = json_decode($data, true);
+  //print_r($data_array);
+  if (!empty($data_array) $data_array['status'] != 404) {
+    
+    ?>
 <tr>
 <?php
-if ($page_type == 'tx_report') {
-?>
+  if ($page_type == 'tx_report') {
+    ?>
 <th>Timestamp</th>
 <th>txid</th>
 <?php
-} else {
-?>
+  } else {
+    ?>
 <th>Timestamp</th>
 <th>Server</th>
 <th>Request</th>
 <th>User Agent</th>
 <th>CSNUtID</th>
 <?php
-}
-?>
+  }
+  ?>
 </tr>
 <tr>
 <?php
-$data_array = json_decode($data, true);
-if ($page_type == 'tx_report') {
-  foreach ($data_array['hits']['hits'] as $hit) {
-    $hit = $hit['_source'];
-    echo "<td>" . $hit['@timestamp'] . "</td>\n";
-    echo "<td><a href='/view_detail.php?txid=" . $hit['@fields']['txid'] . "'>" . $hit['@fields']['txid'] . "</a></td>\n";
-    echo "<tr>";
+  
+  if ($page_type == 'tx_report') {
+    foreach ($data_array['hits']['hits'] as $hit) {
+      $hit = $hit['_source'];
+      echo "<td>" . $hit['@timestamp'] . "</td>\n";
+      echo "<td><a href='/view_detail.php?txid=" . $hit['@fields']['txid'] . "'>" . $hit['@fields']['txid'] . "</a></td>\n";
+      echo "<tr>";
+    }
+  } else {
+    foreach ($data_array['hits']['hits'] as $hit) {
+      $hit = $hit['_source'];
+      echo "<td>" . $hit['@timestamp'] . "</td>\n";
+      echo "<td>" . $hit['@source'] . "</td>\n";
+      echo "<td>" . $hit['@fields']['request'] . "</td>\n";
+      echo "<td>" . $hit['@fields']['http_user_agent'] . "</td>\n";
+      echo "<td><a href='/view_detail.php?csnutid=" . $hit['@fields']['CSNUtID'] . "'>" . $hit['@fields']['CSNUtID'] . "</a></td>\n";
+      echo "<tr>";
+    }
   }
-} else {
-  foreach ($data_array['hits']['hits'] as $hit) {
-    $hit = $hit['_source'];
-    echo "<td>" . $hit['@timestamp'] . "</td>\n";
-    echo "<td>" . $hit['@source'] . "</td>\n";
-    echo "<td>" . $hit['@fields']['request'] . "</td>\n";
-    echo "<td>" . $hit['@fields']['http_user_agent'] . "</td>\n";
-    echo "<td><a href='/view_detail.php?csnutid=" . $hit['@fields']['CSNUtID'] . "'>" . $hit['@fields']['CSNUtID'] . "</a></td>\n";
-    echo "<tr>";
+  } else {
+    echo 'No logs found!';
   }
-}
-?>
+  
+  ?>
 </table>
-<?
-include "awesome_footer.php";
-?>
+
