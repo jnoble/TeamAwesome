39b2b1b4d7588c9a33c92b557d44a8ed349d225c
Chris McCoy <chris@brownbagcode.com>
2013-07-14 23:09:12 -0400

    turning off logs

--- vagrant_getting_started/www/index.php
+++ vagrant_getting_started/www/index.php
@@ -1,7 +1,7 @@
 <?php
   include 'awesome_header.php';
   include 'header.php';
-  ini_set('display_errors', E_ALL);
+  ini_set('display_errors', 0);
   include 'view_detail.php';
   include 'awesome_footer.php';
   ?>
